9f59453a751dcaaa4323591eebdb5391f8822bb5
RobertCaicedo3723 <robert2224513@correo.uis.edu.co>
2022-06-06 08:13:40 -0500

    first commit

---
+++ for_1/for_4.py
@@ -0,0 +1,35 @@
+# Caso 7
+
+for i in range(10):
+    print(i)
+
+# Caso 8 
+for i in range(1, 11):
+    print(i)
+
+# Caso 9
+for i in range(1, 11):
+    print("Ing. Sistemas la mejor :)")
+
+# Caso 10
+for i in range(10):
+    print("i = " + str(i))
+
+# Caso 11
+for i in range(10):
+    print(f"i = {i}")
+
+# Caso 12
+
+for i in range(-8, -13, -1):
+    print(f"i = {i}")
+
+# Caso 13
+nombre = input("Digite su nombre: ")
+contador = 0
+vocales = ["a", "e", "i", "o", "u"]
+for vocal in nombre:
+    print(vocal)
+
+
+
